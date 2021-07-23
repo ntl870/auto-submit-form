d379bd561d486d8719f38f50113da922471a7e8c
Long Nguyen <ntlong870@gmail.com>
2021-07-23 19:12:15 +0700

    updated

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ app.enable("trust proxy");
 const axios = require("axios");
 const { wakeDyno } = require("heroku-keep-awake");
 const request = require("request");
+
+const port = process.env.PORT || 7000;
 app.listen(port, () => {
   console.log("Server started successfully", port);
   wakeDyno("https://auto-submit-form.herokuapp.com/");
 });
+
+
 schedule.scheduleJob("20 13 19 * * *", () => {
   // axios.get(
   //   "https://docs.google.com/forms/d/e/1FAIpQLSd9XF2Vuhs6mGPAbw-1n7tKcPu00Mn6i7VHNGCkTy8aHRYoFQ/formResponse?entry.1331773320=Long&entry.140077259=Nguyen"
@@ -20,7 +24,7 @@ schedule.scheduleJob("20 13 19 * * *", () => {
   console.log("submited");
 });
 
-const port = process.env.PORT || 7000;
+
 // const opts = {
 //   stopTimes: { start: "02:00", end: "21:00" },
 // };
