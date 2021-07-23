fcf8816f930bb8b39afaed941a7c9af52502e095
Long Nguyen <ntlong870@gmail.com>
2021-07-23 19:09:24 +0700

    updated

--- app.js
+++ app.js
@@ -2,21 +2,26 @@ const express = require("express");
 const app = express();
 const schedule = require("node-schedule");
 app.enable("trust proxy");
-const { default: axios } = require("axios");
+const axios = require("axios");
 const { wakeDyno, wakeDynos } = require("heroku-keep-awake");
+const request = require("request");
 
-schedule.scheduleJob("40 05 19 * * *", () => {
-  axios.get(
-    "https://docs.google.com/forms/d/e/1FAIpQLSd9XF2Vuhs6mGPAbw-1n7tKcPu00Mn6i7VHNGCkTy8aHRYoFQ/formResponse?entry.1331773320=Long&entry.140077259=Nguyen"
-  );
+schedule.scheduleJob("10 09 19 * * *", () => {
+  // axios.get(
+  //   "https://docs.google.com/forms/d/e/1FAIpQLSd9XF2Vuhs6mGPAbw-1n7tKcPu00Mn6i7VHNGCkTy8aHRYoFQ/formResponse?entry.1331773320=Long&entry.140077259=Nguyen"
+  // );
+  request({
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSd9XF2Vuhs6mGPAbw-1n7tKcPu00Mn6i7VHNGCkTy8aHRYoFQ/formResponse?entry.1331773320=Long&entry.140077259=Nguyen",
+    method: "GET",
+  });
   console.log("submited");
 });
 
 const port = process.env.PORT || 7000;
-const opts = {
-  stopTimes: { start: "02:00", end: "21:00" },
-};
+// const opts = {
+//   stopTimes: { start: "02:00", end: "21:00" },
+// };
 app.listen(port, () => {
   console.log("Server started successfully", port);
-  wakeDyno("https://auto-submit-form.herokuapp.com/", opts);
+  wakeDyno("https://auto-submit-form.herokuapp.com/");
 });
