1afa81542ee1f60952b7e4a72711d5bdb71f5ed2
Long Nguyen <ntlong870@gmail.com>
2021-07-23 18:38:22 +0700

    updated

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const app = express();
-const url = require("./url");
 const schedule = require("node-schedule");
 app.enable("trust proxy");
-const request = require("request");
+const { default: axios } = require("axios");
 
-schedule.scheduleJob("00 00 01 * * *", () => {
-  request({
-    url: url,
-    method: "GET",
-  });
-  console.log("submited");
+schedule.scheduleJob("40 37 18 * * *", () => {
+  try {
+    axios.get(
+      "https://docs.google.com/forms/d/e/1FAIpQLSd9XF2Vuhs6mGPAbw-1n7tKcPu00Mn6i7VHNGCkTy8aHRYoFQ/formResponse?entry.1331773320=Long&entry.140077259=Nguyen"
+    );
+    console.log("submited");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-let port = process.env.PORT;
-if (!port) {
-  port = 7000;
-}
+const port = process.env.PORT || 7000;
 
 app.listen(port, () => {
   console.log("Server started successfully");
