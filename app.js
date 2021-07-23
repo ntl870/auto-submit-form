06d4182edd5009bd5afee837839b5704ffbfd671
Long Nguyen <ntlong870@gmail.com>
2021-07-23 20:05:09 +0700

    updated

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ schedule.scheduleJob("00 00 01 * * *", () => {
     console.log(err);
   }
 });
-
+schedule.scheduledJobs("00 30 * * * *", () => {
+  console.log("Awake");
+});
 const port = process.env.PORT || 7000;
 const opts = {
   stopTimes: { start: "02:00", end: "21:00" },
