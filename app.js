6439002ab125ba23e46c1d0ce651688447f11ef0
Long Nguyen <ntlong870@gmail.com>
2021-07-24 01:02:28 +0700

    updated

--- app.js
+++ app.js
@@ -16,12 +16,16 @@ schedule.scheduleJob("00 00 01 * * *", () => {
     console.log(err);
   }
 });
+const opts = {
+  logging: true,
+  stopTimes: { start: "02:00", end: "21:00" },
+};
 schedule.scheduleJob("00 30 * * * *", () => {
-  wakeDyno("https://auto-submit-form.herokuapp.com/");
+  wakeDyno("https://auto-submit-form.herokuapp.com/", opts);
 });
 const port = process.env.PORT || 7000;
 
 app.listen(port, () => {
   console.log("Server started successfully", port);
-  wakeDyno("https://auto-submit-form.herokuapp.com/");
+  wakeDyno("https://auto-submit-form.herokuapp.com/", opts);
 });
